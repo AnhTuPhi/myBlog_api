3b9b2a7df4b1a4f1784d2d20927618384125c7db
dev1 <dev1@oceantech.vn>
2021-09-26 01:01:21 +0700

    first commit include base object

---
+++ src/main/java/com/example/blog/dto/request/SignupRequest.java
@@ -0,0 +1,79 @@
+package com.example.blog.dto.request;
+
+import java.util.Date;
+import java.util.Set;
+
+public class SignupRequest {
+    private String username;
+    private String email;
+    private Set<String> role;
+    private String password;
+    private Integer age;
+    private String phoneNumber;
+    private Date createDate;
+    private Date modifyDate;
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public Set<String> getRole() {
+        return this.role;
+    }
+
+    public void setRole(Set<String> role) {
+        this.role = role;
+    }
+
+    public Integer getAge() {
+        return age;
+    }
+
+    public void setAge(Integer age) {
+        this.age = age;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    public Date getCreateDate() {
+        return createDate;
+    }
+
+    public void setCreateDate(Date createDate) {
+        this.createDate = createDate;
+    }
+
+    public Date getModifyDate() {
+        return modifyDate;
+    }
+
+    public void setModifyDate(Date modifyDate) {
+        this.modifyDate = modifyDate;
+    }
+}
---
+++ src/main/java/com/example/blog/repository/RoleRepository.java
@@ -0,0 +1,15 @@
+package com.example.blog.repository;
+
+import com.example.blog.entity.ERole;
+import com.example.blog.entity.Role;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface RoleRepository extends JpaRepository<Role, Long> {
+    Optional<Role> findByName(ERole name);
+}
+
+
